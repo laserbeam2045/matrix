a7f548c594400446e12f60807caffd6f1f594b3f
laserbeam2045 <laserbeam2045@gmail.com>
2021-01-05 07:54:33 +0900

    fds

--- src/api/db.php
+++ src/api/db.php
@@ -12,12 +12,4 @@ if (TEST_MODE):
     // エミュレートモードをOFFにする(数値型を文字列型にさせないため)
     PDO::ATTR_EMULATE_PREPARES => false,
   ]);
-else:
-  define('DSN', 'mysql:host=mysql101.phy.lolipop.lan;dbname=LAA0723421-database;');
-  define('USERNAME', 'LAA0723421');
-  define('PASSWORD', 'hide2631');
-  define('PDO_OPTION', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-  ]);
 endif;
